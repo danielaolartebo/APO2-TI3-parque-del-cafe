a3195ac18d586f0547d9f35f0ebc1d61d124fef9
Santiago Gutiérrez <santicoguvi02@gmail.com>
2021-05-25 23:15:14 -0500

    advanges

--- src/model/Games.java
+++ src/model/Games.java
@@ -1,5 +1,16 @@
 package model;
 
 public class Games {
-
+	
+	private String games;
+	private LineLinkedList first;
+	private LineLinkedList last;
+	
+	public Games(String games) {
+		this.games=games;
+	}
+	
+	
+	public
+	
 }
