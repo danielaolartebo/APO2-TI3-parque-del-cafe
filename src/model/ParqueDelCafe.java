6b10fbbc57a15d12401200f61fa1bd6b29cdf7bb
Santiago Gutiérrez <santicoguvi02@gmail.com>
2021-05-30 21:41:45 -0500

    Fix some problems

--- src/model/ParqueDelCafe.java
+++ src/model/ParqueDelCafe.java
@@ -4,83 +4,84 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-	public class ParqueDelCafe implements Serializable{
+public class ParqueDelCafe implements Serializable{
 		
-		private static final long serialVersionUID = 1;
+	private static final long serialVersionUID = 1;
 		
-		private int planTotalPrice;
-		private List<CustomerAccount> customers;
-		private List<Game> games;
-		private List<FoodCourt> foods;
-		private List<Parking> parkings;
-		//Games
-		private Game rollerCoaster;
-		private Game karts;
-		private Game wheel;
-		private Game carousel;
-		private Game krater;
-		private Game crashingBoats;
-		private Game fast;
-		private Game yipe;
-		private Game mountain;
-		//Food court
-		private FoodCourt heladerias;
-		private FoodCourt subway;
-		private FoodCourt parrilla;
-		private FoodCourt guadual;
-		//user
-		private CustomerAccount customer;
+	private int planTotalPrice;
+	private List<CustomerAccount> customers;
+	private List<Game> games;
+	private List<FoodCourt> foods;
+	private List<Parking> parkings;
+	//Games
+	private Game rollerCoaster;
+	private Game karts;
+	private Game wheel;
+	private Game carousel;
+	private Game krater;
+	private Game crashingBoats;
+	private Game fast;
+	private Game yipe;
+	private Game mountain;
+	//Food court
+	private FoodCourt heladerias;
+	private FoodCourt subway;
+	private FoodCourt parrilla;
+	private FoodCourt guadual;
+	//user
+	private CustomerAccount customer;
 		
-		public ParqueDelCafe() {
-			customers = new ArrayList<>();
-			games = new ArrayList<>();
-			foods = new ArrayList<>();
-			parkings = new ArrayList<>();
-			rollerCoaster = new Game("Monta�a Rusa");
-			karts = new Game("Karts");
-			wheel = new Game("Rueda");
-			carousel = new Game("Carusel");
-			krater = new Game("Krater");
-			crashingBoats = new Game("Botes Chocones");
-			fast = new Game("Rapidos");
-			yipe = new Game("Yipe");
-			mountain = new Game("Monta�a");
-			heladerias = new FoodCourt("Heladerias del Parque");
-			subway = new FoodCourt("Subway");
-			parrilla = new FoodCourt("Parrilla del Parque");
-			guadual = new FoodCourt("El Guadual");
-			planTotalPrice = 0;
-		}
+	public ParqueDelCafe() {
+		customers = new ArrayList<>();
+		games = new ArrayList<>();
+		foods = new ArrayList<>();
+		parkings = new ArrayList<>();
+		rollerCoaster = new Game("Monta�a Rusa");
+		karts = new Game("Karts");
+		wheel = new Game("Rueda");
+		carousel = new Game("Carusel");
+		krater = new Game("Krater");
+		crashingBoats = new Game("Botes Chocones");
+		fast = new Game("Rapidos");
+		yipe = new Game("Yipe");
+		mountain = new Game("Monta�a");
+		heladerias = new FoodCourt("Heladerias del Parque");
+		subway = new FoodCourt("Subway");
+		parrilla = new FoodCourt("Parrilla del Parque");
+		guadual = new FoodCourt("El Guadual");
+		planTotalPrice = 0;
+	}
 		
-		public void addCustomer(String userName, String password, String name, String gender, String age) {
-			customers.add(new CustomerAccount(userName, password, name, gender, age));
+	public void addCustomer(String userName, String password, String name, String gender, String age) {
+		customers.add(new CustomerAccount(userName, password, name, gender, age));
 			
-		}
+	}
 		
-		public List<CustomerAccount> getCustomers(){
-			return customers; 
-		}
+	public List<CustomerAccount> getCustomers(){
+		return customers; 
+	}
 		
-		public boolean validateCustomer(String userName, String password) {
-			boolean validate=false;
-			for(int i=0; i<customers.size() && !validate;i++) {
-				CustomerAccount customer = customers.get(i);
-				if(customer.getUserName().equals(userName) && customer.getPassword().equals(password)) {
-					validate=true;
-				}
+	public boolean validateCustomer(String userName, String password) {
+		boolean validate=false;
+		for(int i=0; i<customers.size() && !validate;i++) {
+			CustomerAccount customer = customers.get(i);
+			if(customer.getUserName().equals(userName) && customer.getPassword().equals(password)) {
+				validate=true;
 			}
+		}
 			return validate;
 		}
 		
-		public CustomerAccount findCustomer(String userName){
-			CustomerAccount tempName=null;
-			for (int i=0; i < customers.size();i++) {
-				if(customers.get(i).getUserName().equals(userName)) {
+	public CustomerAccount findCustomer(String userName){
+		CustomerAccount tempName=null;
+		for (int i=0; i < customers.size();i++) {
+			if(customers.get(i).getUserName().equals(userName)) {
 					tempName = customers.get(i);
-				}
 			}
-			return tempName;
 		}
+		return tempName;
+	}
+		
 	public void setUser(String userName) {
 		
 		CustomerAccount tmp = findCustomer(userName);
